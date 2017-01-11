80200080171f9a95f2bf772f0625af52294c7958
kf2539398 <keithfrnndz@gmail.com>
2017-01-10 21:01:38 -0800

    Signed-off-by: kf2539398 <keithfrnndz@gmail.com>

---
+++ Homework/Assignment_1/Chapter 2 Problem 1/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Keith Fernandez
+ *
+ * Created on January 10, 2017, 3:21 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    int small;
+    int big;
+    int total;
+    
+    
+    small = 62;
+    big = 99;
+    total = small +  big;
+    
+           
+    cout <<"total is equal to big + small, which is " << total << endl;
+
+    return 0;
+}
+
---
+++ Homework/Assignment_1/Chapter 2 Problem 2/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Keith Fernandez
+ *
+ * Created on January 10, 2017, 8:31 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ * 
+ */
+int main() {
+    int revenue;
+    int sales;
+    
+    sales = 4.6E6;
+    revenue = sales * .62;
+    
+    
+    cout << "total revenue for the year is $" << revenue << endl;
+    return 0;
+}
+
